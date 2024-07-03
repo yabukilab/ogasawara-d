327e022373f7da433c37ced3a0a2b386da4400e5
Keisuke <s2242019wc@s.chibakoudai.jp>
2024-07-03 15:36:22 +0900

    ダンプ

--- mydb.sql
+++ mydb.sql
@@ -1,50 +1,79 @@
-DROP DATABASE if exists mydb;
-
-CREATE DATABASE mydb;
-grant all on mydb.* to testuser@localhost identified by 'pass';
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+-- MariaDB dump 10.19  Distrib 10.4.32-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: mydb
+-- ------------------------------------------------------
+-- Server version	10.4.32-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-#SQL文1
+--
+-- Table structure for table `detail`
+--
 
-USE mydb;
+DROP TABLE IF EXISTS `detail`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `detail` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `img` mediumblob DEFAULT NULL,
+  `text` text DEFAULT NULL,
+  `give` tinyint(1) DEFAULT 0,
+  `get` tinyint(1) DEFAULT 0,
+  `owner` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-DROP TABLE if exists logininf;
+--
+-- Dumping data for table `detail`
+--
 
-CREATE TABLE logininf (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
-);
+LOCK TABLES `detail` WRITE;
+/*!40000 ALTER TABLE `detail` DISABLE KEYS */;
+/*!40000 ALTER TABLE `detail` ENABLE KEYS */;
+UNLOCK TABLES;
 
-#SQL文2
-USE mydb;
+--
+-- Table structure for table `logininf`
+--
 
-DROP TABLE if exists detail;
+DROP TABLE IF EXISTS `logininf`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `logininf` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE detail (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    img mediumblob DEFAULT NULL,
-    
-    text TEXT DEFAULT NULL
-);
+--
+-- Dumping data for table `logininf`
+--
 
-#SQL文3: detailテーブルにgiveカラムとgetカラムを追加
-ALTER TABLE detail ADD COLUMN give TINYINT(1) DEFAULT 0;
-ALTER TABLE detail ADD COLUMN get TINYINT(1) DEFAULT 0;
+LOCK TABLES `logininf` WRITE;
+/*!40000 ALTER TABLE `logininf` DISABLE KEYS */;
+/*!40000 ALTER TABLE `logininf` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-#SQL文4: detailテーブルにownerカラムを追加
-ALTER TABLE detail ADD COLUMN owner INT;
-
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-07-03 15:35:26
