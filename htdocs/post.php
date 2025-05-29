5526e3986ec06ca26162f89e45fcfa2ee234c3e0
ogawaha <s2342037wv@s.chibakoudai.jp>
2025-05-29 17:29:43 +0900

    エラー修正

--- htdocs/post.php
+++ htdocs/post.php
@@ -27,5 +27,34 @@ $prepare->bindValue(':id', $id, PDO::PARAM_STR);
 
 $prepare->execute(); # 実行
 ?>
+
+<table>
+  <caption>全データ</caption>
+  <tr>
+    <th>食材名</th>
+    <th>g/個</th>
+    <th>賞味期限</th>
+  </tr>
+<?php                              
+$sql = 'SELECT * FROM food';                  # SQL文
+$prepare = $db->prepare($sql);                  # 準備
+$prepare->execute();                            # 実行
+$result = $prepare->fetchAll(PDO::FETCH_ASSOC); # 結果の取得
+
+foreach ($result as $row) {
+  if($row['id']=$id)
+  $name = h($row['name']);
+  $g = h($row['g']);
+  $y = h($row['y']);
+  $m = h($row['m']);
+  $d = h($row['d']);
+  echo '<tr>' .
+    "<td>{$name}</td>".
+    "<td>{$g}</td>".
+    "<td>{$y}年{$m}月{$d}日</td>".
+    '</tr>';
+}
+?>
+</table>
   </body>
 </html>
