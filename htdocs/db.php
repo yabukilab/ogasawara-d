c9aed48aa85e8dce8cce45f8981860280dd73e2d
ogawaha <s2342037wv@s.chibakoudai.jp>
2025-06-27 18:51:42 +0900

    Delete unnecessary files

--- htdocs/db.php
+++ htdocs/db.php
@@ -14,6 +14,8 @@ $dbUser = isset($_SERVER['MYSQL_USER'])     ? $_SERVER['MYSQL_USER']     : 'test
 $dbPass = isset($_SERVER['MYSQL_PASSWORD']) ? $_SERVER['MYSQL_PASSWORD'] : 'pass';
 $dbName = isset($_SERVER['MYSQL_DB'])       ? $_SERVER['MYSQL_DB']       : 'mydb';
 
+
+
 $dsn = "mysql:host={$dbServer};dbname={$dbName};charset=utf8";
 
 try {
